26ea7b954759548f7ed0164c666539dc496fc873
Martin English <martin.english@basissap.com>
2025-01-06 19:19:50 +1100

    Initial load

---
+++ dropbox-index.bat
@@ -0,0 +1,11 @@
+REM 
+REM python C:\Program Files\Python\Python36\Tools\scripts\2to3.py -w dropbox-index.py
+REM 
+REM This one ...
+REM    py T:\dropbox-index.py -R -T T:\dropbox-template.html T:\dropbox-icons\.. T:\
+REM
+pause
+T:
+cd T:\
+py T:\dropbox-index.py -R -T T:\dropbox-template.html T:\dropbox-icons\.. T:\
+pause
---
+++ index-dropbox.bat
@@ -0,0 +1,6 @@
+REM https://www.youtube.com/watch?v=8qxKYnAsNuU
+REM cd c:\Users\Martin\SkyDrive\dropbox-index>
+REM python C:\Users\Martin\AppData\Local\Programs\Python\Python35\Tools\scripts\2to3.py -w dropbox-index.py
+REM
+python dropbox-index.py -R -T template-dropbox.html "..\Public"
+pause
